dbc94871c22f1bc26335a3157b121ccfc4cda864
Zhen Wei Wu <zhenwu93@gmail.com>
2018-09-15 19:45:23 +0000

    automatically backed up by learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -31,19 +31,13 @@ end
 def play(songs)
   puts "Please enter a song name or number:"
   song_input = gets.chomp
-  
+
 end
 
 def list(song)
-  puts "1. Phoenix - 1901
-2. Tokyo Police Club - Wait Up
-3. Sufjan Stevens - Too Much
-4. The Naked and the Famous - Young Blood
-5. (Far From) Home - Tiga
-6. The Cults - Abducted
-7. Phoenix - Consolation Prizes
-8. Harry Chapin - Cats in the Cradle
-9. Amos Lee - Keep It Loose, Keep It Tight"
+songs.each do |song, location|
+  puts song
+  end
 end
 
 def exit_jukebox
