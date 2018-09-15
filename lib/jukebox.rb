4c9d418e2db22abdb07663117fff1bc60ce2b702
Zhen Wei Wu <zhenwu93@gmail.com>
2018-09-15 19:44:41 +0000

    automatically backed up by learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -21,17 +21,17 @@ songs = [
 #puts say_hello(users_name)
 
 def help
-  puts "I accept the following commands:
-- help : displays this help message
-- list : displays a list of songs you can play
-- play : lets you choose a song to play
-- exit : exits this program"
+  puts "I accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
 end
 
 def play(songs)
   puts "Please enter a song name or number:"
   song_input = gets.chomp
-
+  
 end
 
 def list(song)
